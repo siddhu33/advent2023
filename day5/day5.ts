58f1e9e6571fd723e0d806c9ace5d070f8c213bb
Siddharth Vasu <siddharth_vasu@hotmail.co.uk>
2023-12-05 20:31:31 +0000

    refactor day5 so its cleaner

--- day5/day5.ts
+++ day5/day5.ts
@@ -14,33 +14,40 @@ const seeds = lines[0]
   .split("seeds: ", 2)[1]
   .split(" ")
   .map((v) => parseInt(v, 10));
-const maps: Destination[][] = [];
-let map: Destination[] = [];
-for (let i = 1; i < lines.length; i++) {
-  const line = lines[i];
-  if (line != "") {
-    if (line.includes("map:")) {
-      if (map != null && map.length) {
-        maps.push(JSON.parse(JSON.stringify(map)));
+const mapsFromFile = getMapsFromFile();
+
+function getMapsFromFile() {
+  const maps: Destination[][] = [];
+  let map: Destination[] = [];
+  for (let i = 1; i < lines.length; i++) {
+    const line = lines[i];
+    if (line != "") {
+      if (line.includes("map:")) {
+        add_new_map(maps, map);
+        map = [];
+      } else {
+        //map range into destinations
+        const [dest_min, source_min, offset] = line
+          .split(" ", 3)
+          .map((i) => parseInt(i, 10));
+        map.push({
+          source_min: source_min,
+          source_max: source_min + offset - 1,
+          dest_min: dest_min,
+          dest_max: dest_min + offset - 1,
+          offset: offset,
+        });
       }
-      map = [];
-    } else {
-      //map range into destinations
-      const [dest_min, source_min, offset] = line
-        .split(" ", 3)
-        .map((i) => parseInt(i, 10));
-      map.push({
-        source_min: source_min,
-        source_max: source_min + offset - 1,
-        dest_min: dest_min,
-        dest_max: dest_min + offset - 1,
-        offset: offset,
-      });
     }
   }
+  add_new_map(maps, map);
+  return maps;
 }
-if (map != null && map.length) {
-  maps.push(JSON.parse(JSON.stringify(map)));
+
+function add_new_map(maps: Destination[][], map: Destination[]) {
+  if (map != null && map.length) {
+    maps.push(JSON.parse(JSON.stringify(map)));
+  }
 }
 
 function mapValue(input: number, map: Destination[]): number {
@@ -56,7 +63,7 @@ function part1() {
   let min_value: number | undefined = undefined;
   seeds.forEach((seed) => {
     let value = seed;
-    for (const map of maps) {
+    for (const map of mapsFromFile) {
       value = mapValue(value, map);
     }
     min_value = min_value != undefined ? Math.min(min_value, value) : value;
@@ -69,10 +76,9 @@ function part2() {
   for (let i = 0; i < seeds.length - 1; i += 2) {
     const seed = seeds[i];
     const range = seeds[i + 1];
-    console.log(seed);
     for (let j = 0; j < range; j++) {
       let value = seed + j;
-      for (const map of maps) {
+      for (const map of mapsFromFile) {
         value = mapValue(value, map);
       }
       min_value = min_value != undefined ? Math.min(min_value, value) : value;
